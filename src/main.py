e4bee0f67929362121692b5e906036d51775935f
ATISHAY JAIN <atishayj.dev@gmail.com>
2024-09-08 15:27:21 +0530

    bumpversion

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ from src.router.message import message_router
 from src.router.user import user_router
 from src.utils.client import getDBClient, getFirebaseClient
 
-app = FastAPI(title="SIH Main PS Backend", version="0.1.1-dev1")
+app = FastAPI(title="SIH Main PS Backend", version="0.1.1-dev2")
 
 origins = os.environ["CORS_ORIGINS"].split(",")
 
