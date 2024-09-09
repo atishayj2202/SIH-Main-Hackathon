49072bf99e2a53b26fa3f18b83942d4bb6aaac4e
ATISHAY JAIN <atishayj.dev@gmail.com>
2024-09-09 22:45:23 +0530

    added ai category wise

--- src/router/message.py
+++ src/router/message.py
@@ -11,9 +11,31 @@ message_router = APIRouter(prefix=MESSAGE_PREFIX)
 
 GET_MESSAGES = "/get-messages/"
 GET_AI_REPLY = "/get-ai-reply/"
+GET_CATEGORY_MESSAGES = "/{category_id}/get-messages/"
+GET_CATEGORY_AI_REPLY = "/{category_id}/get-ai-reply/"
 
 
 @message_router.get(GET_MESSAGES, response_model=list[MessageResponse])
+async def get_messages(
+    verified_user: VerifiedUser = Depends(verify_user),
+    db_client: DBClient = Depends(getDBClient),
+):
+    return MessageService.get_all_messages(
+        user=verified_user.requesting_user, db_client=db_client
+    )
+
+
+@message_router.post(GET_AI_REPLY, response_model=MessageResponse)
+async def get_ai_reply(
+    request: MessageRequest,
+    verified_user: VerifiedUser = Depends(verify_user),
+    db_client: DBClient = Depends(getDBClient),
+):
+    return MessageService.get_ai_reply(
+        request=request, user=verified_user.requesting_user, db_client=db_client
+    )
+
+@message_router.get(GET_CATEGORY_MESSAGES, response_model=list[MessageResponse])
 async def get_messages(
     verified_user: VerifiedUser = Depends(verify_user),
     db_client: DBClient = Depends(getDBClient),
